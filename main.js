f597850b63950fa4809551f6419aad3350b8c68f
Krisztina Szabo <andrekrisztina5@gmail.com>
2020-01-23 18:07:32 +0000

    film part fixed

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
-function getData(cb) {
-var xhr = new XMLHttpRequest();
-var data;
+const baseURL = "https://swapi.co/api/";
 
-xhr.open("GET", "https://swapi.co/api/");
-xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        cb(JSON.parse(this.responseText));
-    }
-};
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
-function printDataToConsole(data){
-    console.log(data);
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
 }
-getData(printDataToConsole);
 
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
 
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            // el.innerHTML += "<p>" + item.name + "</p>";
+        });
+
+        el.innerHTML = `<table>${tableHeaders}</table>`;
+    });
+}
